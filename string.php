2c1ec0f208b87e6fda94e3197d04fdb6964eef0f
AnikHaque <ekramul15-811@diu.edu.bd>
2025-06-09 22:21:35 +0600

    problem 1 done

--- string.php
+++ string.php
@@ -2,4 +2,15 @@
 
 $strings = ["Hello", "World", "PHP", "Programming"];
 
+foreach ($strings as $string) {
+    // Count vowels
+    $vowelCount = preg_match_all('/[aeiouAEIOU]/', $string);
+    
+    // Reverse string
+    $reversed = strrev($string);
+
+    // Output
+    echo "Original String: $string, Vowel Count: $vowelCount, Reversed String: $reversed\n";
+}
+
 ?>
